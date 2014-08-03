f7ab0a8b7d495eb132de038247ce3ee161eabfde
mrgreaper <mrgreaper2004@gmail.com>
2014-08-03 14:54:09 +0100

    added stuff...edited stuff...stuff happened

---
+++ src/main/java/com/mrgreaper/twistedmod2/gui/GuiSpeaky.java
@@ -0,0 +1,24 @@
+package com.mrgreaper.twistedmod2.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
+
+/**
+ * Created by david on 03/08/2014.
+ */
+public class GuiSpeaky extends GuiScreen {
+    //todo finish this
+    public void intGui() {
+        //textfield = new GuiTextField(this,fontRenderer,20,20,20,100,"");
+        // textfield.isFocused = false;
+    }
+
+
+    public void drawScreen(int x, int y, float f) {
+
+        super.drawScreen(x, y, f);
+    }
+
+    private GuiTextField textfield;
+
+}
---
+++ src/main/java/com/mrgreaper/twistedmod2/items/cardSpeaky.java
@@ -0,0 +1,9 @@
+package com.mrgreaper.twistedmod2.items;
+
+/**
+ * Created by david on 03/08/2014.
+ */
+public class cardSpeaky extends twistedItems {
+
+
+}
